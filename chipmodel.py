4da6cc8b58874a9ba71a053a46d493948ea5b53f
wdKenish <147270528+wdKenish@users.noreply.github.com>
2024-04-17 00:01:13 +0800

    Update chipmodel.py

--- chipmodel.py
+++ chipmodel.py
@@ -208,6 +208,195 @@ def tug_force(power_force,power_angle):
     N_tug = 
     return X_tug,Y_tug,N_tug
 
+
+In [ ]:
+# 控制模块
+def goalstright():
+    # 刚开始可以进行速度的加速
+    if ship_v =0:
+        # 刚开始加速时,拖轮提供最大拖力
+        pass
+    if ship_v < maxv:
+        a = force_tug / ship_mass
+        ship_v += a * dt 
+        # 简化了，应该是对应的u,v,r都有
+        # 但是这里只为了让船舶前进，所以没有r！
+    elif ship_v > maxv:
+        a = force_tug / ship_mass
+        ship_v -= a * dt 
+    else:
+        # 这个时候的拖轮的力就等于环境干扰力的合力可以计算出拖力
+        pass
+    # 我还要输出拖轮力才能计算整个模型的运动，得到速度和位置
+    # 所以一开始的拖轮力的大小得知道
+    # 可在这个模块中进行位置的计算
+    return force_tug,u,v
+
+
+def turnheading():
+    # 刚开始考虑最大角速度旋转
+    a = N_T / I_zz
+    if a > 0.1:
+        a = 0.1
+        r += a * dt
+        N_T = a * I_zz
+    return N_T,r
+
+
+def parallel_berthing(maxpv=0.1):# 最大的平行靠泊速度0.1m/s
+    # 此时两个拖轮共同将船舶平行推向岸边
+    # 所以两个拖轮的作用位置已知，且作用力方向垂直于岸边
+    # 在控制靠泊速度的前提下，可以得到拖轮的推力大小
+    pass
+    
+    
+
+
+
+In [ ]:
+# 算是对船舶运动的前提判断
+
+# 先确定输入值及其意义
+totargetanlge = "东"# 确定船舶停泊后的艏向，即岸的方向和船是左舷靠泊还是右舷靠泊。看来可能要改成判断左右舷靠泊
+# 假设出我需要的值
+angle_ship = 0
+ship_breathd = 18
+initial_position =[0,0]
+initial_v = [0,0]
+target_posiotion = [234,456]
+# 并计算出转向点的位置
+turn_position = [0,0]
+ttt = totargetanlge
+berthing_angle = 0
+ba = berthing_angle
+B = ship_breathd
+
+# 规定船舶的停泊艏向，即规定船舶在靠泊时是左舷靠泊还是右舷靠泊
+# 以及判断船舶的转向点坐标
+if ttt == "东":
+    ba = 0 # 单位都是度
+    if target_posiotion[0] >= 0 and target_posiotion[1]>= 0:
+        # 在第一象限中时
+        target_posiotion[0] -= (4 * B + 20) # 假设的安全平行靠泊距离
+    elif target_posiotion[0] < 0 and target_posiotion[1]>= 0:
+        # 在二
+        target_posiotion[0] += (4 * B + 20)
+    elif target_posiotion[0] < 0 and target_posiotion[1]< 0:
+        # 在三
+        target_posiotion[0] += (4 * B + 20)
+    elif target_posiotion[0] < 0 and target_posiotion[1]>= 0:
+        # 在四
+        target_posiotion[0] -= (4 * B + 20)
+    else:
+        print("不能判断转向点的位置")
+elif ttt =="西":
+    ba = 180
+    if target_posiotion[0] >= 0 and target_posiotion[1]>= 0:
+        # 在第一象限中时
+        target_posiotion[0] -= (4 * B + 20) # 假设的安全平行靠泊距离
+    elif target_posiotion[0] < 0 and target_posiotion[1]>= 0:
+        # 在二
+        target_posiotion[0] += (4 * B + 20)
+    elif target_posiotion[0] < 0 and target_posiotion[1]< 0:
+        # 在三
+        target_posiotion[0] += (4 * B + 20)
+    elif target_posiotion[0] < 0 and target_posiotion[1]>= 0:
+        # 在四
+        target_posiotion[0] -= (4 * B + 20)
+    else:
+        print("不能判断转向点的位置")
+elif ttt =="北":
+    ba = 90
+    if target_posiotion[0] >= 0 and target_posiotion[1]>= 0:
+        # 在第一象限中时
+        target_posiotion[1] -= (4 * B + 20) # 假设的安全平行靠泊距离
+    elif target_posiotion[0] < 0 and target_posiotion[1]>= 0:
+        # 在二
+        target_posiotion[1] -= (4 * B + 20)
+    elif target_posiotion[0] < 0 and target_posiotion[1]< 0:
+        # 在三
+        target_posiotion[1] += (4 * B + 20)
+    elif target_posiotion[0] < 0 and target_posiotion[1]>= 0:
+        # 在四
+        target_posiotion[1] += (4 * B + 20)
+    else:
+        print("不能判断转向点的位置")
+elif ttt =="南":
+    ba = -90
+    if target_posiotion[0] >= 0 and target_posiotion[1]>= 0:
+        # 在第一象限中时
+        target_posiotion[1] -= (4 * B + 20) # 假设的安全平行靠泊距离
+    elif target_posiotion[0] < 0 and target_posiotion[1]>= 0:
+        # 在二
+        target_posiotion[1] -= (4 * B + 20)
+    elif target_posiotion[0] < 0 and target_posiotion[1]< 0:
+        # 在三
+        target_posiotion[1] += (4 * B + 20)
+    elif target_posiotion[0] < 0 and target_posiotion[1]>= 0:
+        # 在四
+        target_posiotion[1] += (4 * B + 20)
+else:
+    print("不能决定船舶的靠泊艏向")
+
+# 判断的变量
+
+a0 = initial_position[0]
+b0 = initial_position[1]
+a1 = turn_position[0]
+b1 = turn_position[1]
+a = target_posiotion[0]
+b = target_posiotion[1]
+# Dit : D initial to turn 
+Dit = ((a0-a1)**2 + (b0-b1)**2) **0.5
+# Dtt : D turn to target 
+Dtt = ((a-a1)**2 + (b-b1)**2) **0.5
+#
+angle_to_turn = (b1 - b0)/(a1 - a)
+att = angle_to_turn
+update_position =[]
+update_v = []
+# 船舶运动从初始位置运动到转向点的判断
+while True:
+    # 首先判断初始位置
+    if math.tan(angle_ship) - att > 0.001:
+        turnheading()
+    elif math.tan(angle_ship) - att <= 0.001:
+        goalstright()
+    # 更新船舶的坐标位置
+    update_position.append(position)
+    # 更新船舶的速度
+    update_v.append(u,v,r)
+    # 得到最后一个坐标的位置
+    a3 = update_position[-1][0]
+    b3 = update_position[-1][1]
+    # 计算最后的坐标位置到转向点的距离
+    distance = ((a3-a1)+(b3-b1))**0.5
+    if distance<=0.001:
+        break
+
+# 此时的船舶艏向就是船舶前进时的艏向
+# 重新赋值给船的艏向
+angle_ship = math.arctan(att)
+turn_angle = angle_ship - ba
+if ba == 0:#逆时针旋转
+    turnheading()
+    pass
+elif ba == 180:
+    # 顺时针旋转
+    turnheading()
+    pass
+elif ba == 90:
+    # 逆时针旋转
+    turnheading()
+    pass
+else:# 顺时针旋转
+    turnheading()
+    pass
+
+# 旋转过turn_angle之后与岸平行，进行平行靠泊
+parallel_berthing()
+
+
 # 仿真计算部分
 X_hull,Y_hull,N_hull = hull_force()
 X_wind,Y_wind,N_wind = wind_force()
